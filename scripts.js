c6014b0f9701f24207c521909f06dde350204136
Praveen K <pravee92tech@gmail.com>
2022-01-09 13:23:51 +0530

    html day 1

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// function showSomeMsg (){
+//   let myDivId = document.getElementById('id-div');  
+//   let MyShowDivId = document.getElementById('id-show-Msg');
+//   myDivId.innerHTML= 'Hi, All';
+//   MyShowDivId.innerHTML= 'Welocme to VIPRANT TECH';
+// }
+
+// function showPreviousAlert(){
+//     alert('Previous Anchor Tag Clicked');
+// }
+// function getAdhaarDetails(url){
+//     ajax.get();
+//     ajax.post()
+//     ajax.delete()
+//     ajax.update();
+// }
+// function showNextAlert(){
+//     // alert('Next Anchor Tag Clicked');
+//     /let myDivId = document.getElementById('id-div');  
+//     // myDivId.innerHTML= 'Next Anchor Tag Clicked';
+//     // myDivId.className="onclick";
+
+//    let myAdhaarDetailsURL =  'http://127.0.0.1:5500/aadharDetails';
+//    let returnedAdhaarDetails = getAdhaarDetails(myAdhaarDetailsURL);
+//     // returned response
+//    returnedAdhaarDetails = ['Praveen', 'Viki', 'Indhu', 'Pradeepa', 'Priyanka'];
+
+//   for(let i=0; i<returnedAdhaarDetails.length; i++){
+    
+//   }
+
+//}
